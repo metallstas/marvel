bfc6fc65dffd76aff83f078231fd985b4bac84ee
Stas Khmylko <metallstas@mail.ru>
2024-10-14 17:02:39 +0300

    add bg

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -5,16 +5,18 @@ import CharacterInfo from '../characterInfo/CharacterInfo'
 
 import './app.scss'
 import '../../style/buttons.scss'
+import bg from '../../resourses/img/bg asset.png'
 
 const App = () => {
     return (
         <div className="app">
             <Header/>
             <RandomChar/>
-            <section className='main-section'>
+            <section className='app__main'>
                 <Characters/>
                 <CharacterInfo/>
             </section>
+            <img className='app__bg' src={bg} alt='superman'/>
         </div>
     )
 }
